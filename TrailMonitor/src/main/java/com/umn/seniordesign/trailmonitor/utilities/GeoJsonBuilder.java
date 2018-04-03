56d7b7eefa724c602f88ff4f9dd729e901746197
Timothy <timothy.mathison96@gmail.com>
2018-04-02 21:59:04 -0500

    working on point to line processing

--- TrailMonitor/src/main/java/com/umn/seniordesign/trailmonitor/utilities/GeoJsonBuilder.java
+++ TrailMonitor/src/main/java/com/umn/seniordesign/trailmonitor/utilities/GeoJsonBuilder.java
@@ -1,6 +1,7 @@
 package com.umn.seniordesign.trailmonitor.utilities;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -46,6 +47,42 @@ public class GeoJsonBuilder {
 		geoJson.setFeatures(features);
 		
 		return geoJson;
+	}
+	
+	public static Map<String, GeoJson> build(Map<Integer, List<TrailPointRecord>> tileRecords, int zoomDepth) throws Exception { 
+		//type "FeatureCollection" which contains a list of features to be plotted
+		GeoJson pointData = new GeoJson(GeoJson.Types.FeatureCollection);
+		GeoJson lineData = new GeoJson(GeoJson.Types.FeatureCollection);
+		
+		
+		Map<String, GeoJson> processedData = new HashMap<String, GeoJson>(2, 1.0F);
+		processedData.put("pointData", pointData);
+		processedData.put("lineData", lineData);
+		return processedData;
+	}
+	
+	public static void processArea(double top, double bot, double left, double right, int divisions, int depth, 
+			List<TrailPointRecord> points, List<Feature> outPoints, List<Feature> outLines) {
+		
+		Bucket[][] grid = new Bucket[divisions][divisions]; //rectangular grid that all points will be mapped into
+		
+	}
+	
+	private class Bucket {
+		private List<TrailPointRecord> points;
+		private int latIndex;
+		private int lngIndex;
+		
+		public Bucket(int latIndex, int lngIndex) {
+			this.points = new LinkedList<TrailPointRecord>();
+			this.latIndex = latIndex;
+			this.lngIndex = lngIndex;
+		}
+		
+		public void add(TrailPointRecord point) {
+			points.add(point);
+		}
+		
 	}
 
 }
