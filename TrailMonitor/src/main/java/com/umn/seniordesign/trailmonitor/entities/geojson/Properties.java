c863c8221d60b77b91d08b9f1814ddba3c60ca61
Timothy <timothy.mathison96@gmail.com>
2018-04-03 10:59:49 -0500

    working on point to line processing

--- TrailMonitor/src/main/java/com/umn/seniordesign/trailmonitor/entities/geojson/Properties.java
+++ TrailMonitor/src/main/java/com/umn/seniordesign/trailmonitor/entities/geojson/Properties.java
@@ -1,14 +1,24 @@
 package com.umn.seniordesign.trailmonitor.entities.geojson;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Properties {
 	private int value;
-	private String deviceId;
+	private Set<String> deviceIds;
 	private long timeStamp;
 	//more fields can be added (customizable)
 	
 	public Properties(int value, String deviceId, long timeStamp) {
 		this.value = value;
-		this.deviceId = deviceId;
+		this.deviceIds = new HashSet<String>(1, 1.0F);
+		this.deviceIds.add(deviceId);
+		this.timeStamp = timeStamp;
+	}
+	
+	public Properties(int value, Set<String> deviceIds, long timeStamp) {
+		this.value = value;
+		this.deviceIds = deviceIds;
 		this.timeStamp = timeStamp;
 	}
 	
@@ -16,8 +26,8 @@ public class Properties {
 		return this.value;
 	}
 	
-	public String getDeviceId() {
-		return this.deviceId;
+	public Set<String> getDeviceIds() {
+		return this.deviceIds;
 	}
 	
 	public long getTimeStamp() {
--- TrailMonitor/src/main/java/com/umn/seniordesign/trailmonitor/utilities/GeoJsonBuilder.java
+++ TrailMonitor/src/main/java/com/umn/seniordesign/trailmonitor/utilities/GeoJsonBuilder.java
@@ -2,11 +2,13 @@ package com.umn.seniordesign.trailmonitor.utilities;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import com.umn.seniordesign.trailmonitor.entities.TrailPointRecord;
 import com.umn.seniordesign.trailmonitor.entities.geojson.Feature;
@@ -64,23 +66,71 @@ public class GeoJsonBuilder {
 	public static void processArea(double top, double bot, double left, double right, int divisions, int depth, 
 			List<TrailPointRecord> points, List<Feature> outPoints, List<Feature> outLines) {
 		
+		double xScale = (right - left) / divisions;
+		double yScale = (top - bot) / divisions;
+		
 		Bucket[][] grid = new Bucket[divisions][divisions]; //rectangular grid that all points will be mapped into
+		List<Bucket> populatedBuckets = new LinkedList<Bucket>();
 		
 	}
 	
 	private class Bucket {
 		private List<TrailPointRecord> points;
-		private int latIndex;
-		private int lngIndex;
+		private int y;
+		private int x;
+		private double totalValues;
+		private Set<String> deviceIds;
+		private long timeStamp;
 		
 		public Bucket(int latIndex, int lngIndex) {
 			this.points = new LinkedList<TrailPointRecord>();
-			this.latIndex = latIndex;
-			this.lngIndex = lngIndex;
+			this.y = latIndex;
+			this.x = lngIndex;
+			this.totalValues = 0;
+			this.timeStamp = 0;
 		}
 		
-		public void add(TrailPointRecord point) {
+		public void add(TrailPointRecord point, boolean process) {
 			points.add(point);
+			if(process) { //combine information from all points in bucket
+				this.totalValues += point.getValue();
+				if(deviceIds == null) {
+					this.deviceIds = new HashSet<String>(50, 1.0F);
+				}
+				this.deviceIds.add(point.getDeviceId());
+				long time = point.getTimeStamp().getTimeInMillis();
+				if(this.timeStamp < time) {
+					this.timeStamp = time;
+				}
+			}
+		}
+		
+		public int size() {
+			return this.points.size();
+		}
+		
+		public int getValue() {
+			return (int)Math.round(this.totalValues / this.size());
+		}
+		
+		public List<TrailPointRecord> getPoints() {
+			return this.points;
+		}
+		
+		public int getX() {
+			return this.x;
+		}
+		
+		public int getY() {
+			return this.y;
+		}
+		
+		public Set<String> getDeviceIds() {
+			return this.deviceIds;
+		}
+		
+		public long getTimeStamp() {
+			return this.timeStamp;
 		}
 		
 	}
